90d58b62bbec86ad22d3529bb62adc75e7357b16
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-04-10 11:53:07 +0300

    add ef samples

---
+++ EFSamples/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFSamples
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Data Source=SZHIZHKONB1\SQLEXPRESS;Initial Catalog=GeneratedLibrary;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ EFSamples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace EFSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using var context = new LibraryContext();
+            context.Database.EnsureCreated();
+
+            Console.Write("what book to search: ");
+            var searchTerm = Console.ReadLine();
+            var _books = context.Books;
+
+            var books = _books
+                .Where(b => b.Author.Contains(searchTerm) || b.Title.Contains(searchTerm));
+            
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Id}\t{book.Title}\t{book.Author}\t{book.PagesCount}");
+            }
+        }
+
+        static void DataAdepterAndCommandBUilder()
+        {
+            var connection = new SqlConnection(GetConnectionString());
+            var dataAdapter = new SqlDataAdapter("SELECT * FROM Books", connection);
+
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            Console.WriteLine($"{nameof(dataAdapter.UpdateBatchSize)}: {dataAdapter.UpdateBatchSize}");
+
+            var booksTable = dataSet.Tables[0];
+            var row = booksTable.NewRow();
+
+            row[1] = "T1";
+            row[2] = "A1";
+            row[3] = 15;
+
+            booksTable.Rows.Add(row);
+
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+            Console.WriteLine($"{nameof(commandBuilder.GetInsertCommand)}: {commandBuilder.GetInsertCommand().CommandText}");
+            Console.WriteLine($"{nameof(commandBuilder.GetUpdateCommand)}: {commandBuilder.GetUpdateCommand().CommandText}");
+            Console.WriteLine($"{nameof(commandBuilder.GetDeleteCommand)}: {commandBuilder.GetDeleteCommand().CommandText}");
+
+
+            dataAdapter.Update(dataSet);
+        }
+
+        private static string GetConnectionString()
+        {
+            return @"Data Source=SZHIZHKONB1\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;";
+        }
+    }
+}
